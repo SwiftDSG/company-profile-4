4a1843b94e89df51337d15d9927e39badd03d28f
Kemal Dwiheldy <33623566+SwiftDSG@users.noreply.github.com>
2022-09-26 22:26:59 +0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'company-profile-4',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+      env: {
+        "NUXT_APP_BASE_URL": "/icoen",
+        "PORT": 3002
+      }
+    }
+  ]
+}
